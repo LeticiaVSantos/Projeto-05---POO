5cb6de9c81616f68eab4a41c4d902a8098a3743c
supremesonicbrazil <34400012+supremesonicbrazil@users.noreply.github.com>
2018-05-10 09:39:28 -0300

    Implementar classes Java Question e esqueleto do Quiz

---
+++ src/java/br/com/fatecpg/webquiz/Quiz.java
@@ -0,0 +1,13 @@
+package br.com.fatecpg.webquiz;
+import java.util.ArrayList;
+
+public class Quiz {
+    private static ArrayList<Question> test;
+    public static ArrayList<Question> getTest(){
+        if (test == null){
+            test = new ArrayList<>();
+            // ADICIONAR QUESTÕES AQUI
+        }
+        return test;
+    }
+}
